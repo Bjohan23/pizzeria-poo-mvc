fc3b508fffb5b5b293e88b04b47c85317d24af16
Bjohan23 <bventurajohanjh@uss.edu.pe>
2024-05-17 12:16:36 -0500

    chore: update app URL for deployment

--- config/app.php
+++ config/app.php
@@ -1,6 +1,7 @@
 <?php
 // const APP_URL = 'http://localhost/pizzeria-poo-mvc/';
-const APP_URL = 'http://192.168.0.103/pizzeria-poo-mvc/';
+// const APP_URL = 'http://192.168.0.103/pizzeria-poo-mvc/';
+const APP_URL = 'https://pizzeria-poo-mvc.onrender.com/';
 const APP_NAME = 'CRUD en PHP POO MVC';
 const APP_SESSION_NAME = 'CRUD_PHP_POO_MVC';
 
